4f37a7af7b209019624bd02c6dcb1fb401c3e4c3
Tahsin Rashad <tahsin.rashad716@gmail.com>
2019-06-16 01:53:57 +0600

    uploading the project in github

---
+++ src/private/firebase-key.ts
@@ -0,0 +1,9 @@
+export const fireBaseKey = {
+    apiKey: "AIzaSyCRytXluFOAm789cYGu0fwq28MhgLjbEOY",
+    authDomain: "oshop-7ad57.firebaseapp.com",
+    databaseURL: "https://oshop-7ad57.firebaseio.com",
+    projectId: "oshop-7ad57",
+    storageBucket: "oshop-7ad57.appspot.com",
+    messagingSenderId: "853515087389",
+    appId: "1:853515087389:web:92d7db07fc3f8263"
+};
